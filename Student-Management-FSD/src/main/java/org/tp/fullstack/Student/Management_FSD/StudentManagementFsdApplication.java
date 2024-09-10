ca4aeeb65c20a3e72686ea3e90f51eab29752ed8
PRAVEEN887 <praveendhamodharncs2020@gmail.com>
2024-09-10 11:29:12 +0530

    .backend java

---
+++ Student-Management-FSD/src/main/java/org/tp/fullstack/Student/Management_FSD/StudentManagementFsdApplication.java
@@ -0,0 +1,13 @@
+package org.tp.fullstack.Student.Management_FSD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementFsdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementFsdApplication.class, args);
+	}
+
+}
---
+++ Student-Management-FSD/src/test/java/org/tp/fullstack/Student/Management_FSD/StudentManagementFsdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tp.fullstack.Student.Management_FSD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementFsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
